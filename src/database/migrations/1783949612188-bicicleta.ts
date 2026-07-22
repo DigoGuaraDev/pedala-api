e8162bcdf04423b74beec3d2f90b711e982cfa6d
digo <dev.digoguara@gmail.com>
2026-07-22 08:19:59 -0300

    B18

--- src/database/migrations/1783949612188-bicicleta.ts
+++ src/database/migrations/1783949612188-bicicleta.ts
@@ -1,22 +1,22 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Bicicleta1783949612188 implements MigrationInterface {
+export class CriarTabelaBicicleta1783949389577 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-         await queryRunner.query(`
-            CREATE TABLE IF  NOT EXISTS bicicletas(
-           id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-           modelo_id UUID NOT NULL,
-           status VARCHAR(20) NOT NULL DEFAULT false,
-           estacao_id UUID NOT NULL
-           dt_cadastro DATE NOT NULL DEFAULT  'now()',
-           dt_atualizar TIMESTAMP,
-           CONSTRAINT fk_modelos_bicicletas FOREIGN KEY (modelo_id)
-             REFERENCES modelos(id) ON UPDATE NO ACTION ON DELETE CASCADE,
-               CONSTRAINT fk_bicicletas_estacao FOREIGN KEY (estacao_id)
-             REFERENCES estacoes(id) ON UPDATE NO ACTION ON DELETE CASCADE
-         );
-         `)
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS bicicletas(
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                modelo_id UUID NOT NULL,
+                status VARCHAR(20) NOT NULL DEFAULT 'DISPONIVEL',
+                estacao_id UUID NOT NULL,
+                dt_cadastro DATE NOT NULL DEFAULT 'now()',
+                dt_atualizacao TIMESTAMP,
+                CONSTRAINT fk_modelo_bicicleta FOREIGN KEY (modelo_id) REFERENCES
+                    modelos(id) ON UPDATE NO ACTION ON DELETE CASCADE,
+                CONSTRAINT fk_bicicleta_estacao FOREIGN KEY (estacao_id) REFERENCES
+                    estacoes(id) ON UPDATE NO ACTION ON DELETE CASCADE
+            );                
+        `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
